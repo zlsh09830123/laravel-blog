b474db404dab13bf5f877c17202ee598e27b3604
zlsh09830123 <zlsh09830123@gmail.com>
2021-01-01 19:16:43 +0800

    初始化樣式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="zh_TW">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Blog')</title>
-</head>
-<body>
-    @yield('content')
-</body>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a href="/" class="navbar-brand">Blog</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a href="/help" class="nav-link">幫助</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">登入</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-    <h1>主頁</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      主頁
+    </p>
+    <p>
+      一切。將從這裡開始
+    </p>
+    <p>
+      <a href="#" class="btn btn-lg btn-success" role="button">現在註冊</a>
+    </p>
+  </div>
 @endsection
