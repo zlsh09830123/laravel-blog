801153ac08e592c4ad4301bff89649a0f4937fcf
zlsh09830123 <zlsh09830123@gmail.com>
2021-01-10 23:24:21 +0800

    正在追蹤和追蹤者列表頁面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,16 +1,16 @@
-<a href="#">
+<a href="{{ route('users.followings', $user->id) }}">
   <strong id="following" class="stat">
     {{ count($user->followings) }}
   </strong>
   追蹤中
 </a>
-<a href="#">
+<a href="{{ route('users.followers', $user->id) }}">
   <strong id="followers" class="stat">
     {{ count($user->followers) }}
   </strong>
   追蹤者
 </a>
-<a href="#">
+<a href="{{ route('users.show', $user->id) }}">
   <strong id="statuses" class="stat">
     {{ $user->statuses()->count() }}
   </strong>
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::get('password/reset/{token}', [PasswordController::class, 'showResetForm'
 Route::post('password/reset', [PasswordController::class, 'reset'])->name('password.update');               // 對提交過來的 Token 和 Email 資料進行配對，正確的話更新密碼
 
 Route::resource('statuses', StatusesController::class, ['only' => ['store', 'destroy']]);
+
+Route::get('users/{user}/followings', [UsersController::class, 'followings'])->name('users.followings');
+Route::get('users/{user}/followers', [UsersController::class, 'followers'])->name('users.followers');
